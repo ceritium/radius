6852db575aafe223691abfa4bd724f515dcd1a63
José Galisteo <ceritium@gmail.com>
2013-04-15 14:44:12 +0200

    fixing por Rails >3.1

--- lib/radius/parser.rb
+++ lib/radius/parser.rb
@@ -7,25 +7,25 @@ module Radius
   class Parser
     # The Context object used to expand template tags.
     attr_accessor :context
-    
+
     # The string that prefixes all tags that are expanded by a parser
     # (the part in the tag name before the first colon).
     attr_accessor :tag_prefix
 
     # The class that performs tokenization of the input string
     attr_accessor :scanner
-  
+
     # Creates a new parser object initialized with a Context.
     def initialize(context = Context.new, options = {})
       if context.kind_of?(Hash) and options.empty?
         options, context = context, (context[:context] || context['context'])
       end
       options = Utility.symbolize_keys(options)
-      self.context = context ? context.dup : Context.new
+      self.context = context ? context.clone : Context.new
       self.tag_prefix = options[:tag_prefix] || 'radius'
       self.scanner = options[:scanner] || default_scanner
     end
-    
+
     # Parses string for tags, expands them, and returns the result.
     def parse(string)
       @stack = [ ParseContainerTag.new { |t| Utility.array_to_s(t.contents) } ]
@@ -33,13 +33,13 @@ module Radius
       stack_up
       @stack.last.to_s
     end
-    
+
     protected
     # Convert the string into a list of text blocks and scanners (tokens)
     def tokenize(string)
       @tokens = scanner.operate(tag_prefix, string)
     end
-    
+
     def stack_up
       @tokens.each do |t|
         if t.is_a? String
